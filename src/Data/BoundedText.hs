f5866f78228459ae5a5f63d9d8ad85fe51751695
Cameron Gera <cameron@flipstone.com>
2025-10-30 13:46:50 -0400

    Adds initial bounded-text package

---
+++ src/Data/BoundedText.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE RoleAnnotations #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Data.BoundedText
+  ( BoundedText
+  , BoundedTextError (..)
+  , describeBoundedTextError
+  , boundedTextFromText
+  , boundedTextToText
+  , boundedTextMaxLength
+  , boundedTextMinLength
+  ) where
+
+import qualified Control.DeepSeq as DeepSeq
+import Data.Proxy (Proxy (Proxy))
+import qualified Data.Text as T
+import GHC.TypeLits (KnownNat, Nat, natVal)
+
+newtype BoundedText (minLen :: Nat) (maxLen :: Nat) = BoundedText T.Text
+  deriving (Eq, Ord, Show)
+
+instance DeepSeq.NFData (BoundedText minLen maxLen) where
+  rnf (BoundedText t) = DeepSeq.rnf t
+
+data BoundedTextError
+  = TextLengthBelowMinimum Integer
+  | TextLengthAboveMaximum Integer
+  deriving (Eq, Show)
+
+describeBoundedTextError :: BoundedTextError -> String
+describeBoundedTextError err =
+  case err of
+    TextLengthBelowMinimum n -> "Text length below minimum: " <> show n
+    TextLengthAboveMaximum n -> "Text length above maximum: " <> show n
+
+type role BoundedText nominal nominal
+
+boundedTextFromText ::
+  forall minLen maxLen.
+  (KnownNat minLen, KnownNat maxLen) =>
+  T.Text ->
+  Either BoundedTextError (BoundedText minLen maxLen)
+boundedTextFromText str =
+  let
+    minVal = natVal (Proxy :: Proxy minLen)
+    maxVal = natVal (Proxy :: Proxy maxLen)
+    len = toInteger (T.length str)
+  in
+    case (len >= minVal, len <= maxVal) of
+      (True, True) -> Right $ BoundedText str
+      (False, _) -> Left $ TextLengthBelowMinimum minVal
+      (_, False) -> Left $ TextLengthAboveMaximum maxVal
+
+boundedTextToText :: BoundedText minLen maxMax -> T.Text
+boundedTextToText (BoundedText txt) = txt
+
+boundedTextMinLength ::
+  forall proxy minLen maxLen.
+  KnownNat minLen =>
+  proxy (BoundedText minLen maxLen) ->
+  Int
+boundedTextMinLength _proxy =
+  fromInteger $ natVal (Proxy :: Proxy minLen)
+
+boundedTextMaxLength ::
+  forall proxy minLen maxLen.
+  KnownNat maxLen =>
+  proxy (BoundedText minLen maxLen) ->
+  Int
+boundedTextMaxLength _proxy =
+  fromInteger $ natVal (Proxy :: Proxy maxLen)
---
+++ test/Main.hs
@@ -0,0 +1,95 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main
+  ( main
+  ) where
+
+import Data.Either (isRight)
+import Data.Proxy (Proxy (Proxy))
+import Hedgehog (Property, forAll, property, withTests, (===))
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import qualified Test.Tasty as Tasty
+import qualified Test.Tasty.Hedgehog as TastyHH
+
+import qualified Data.BoundedText as BoundedText
+
+main :: IO ()
+main =
+  Tasty.defaultMain $
+    Tasty.testGroup
+      "bounded-text"
+      [ TastyHH.testProperty "can build text within bounds" prop_buildsWithinBounds
+      , TastyHH.testProperty "text conversion honors lower bound" prop_respectsLowerBound
+      , TastyHH.testProperty "text conversion honors upper bound" prop_respectsUpperBound
+      , TastyHH.testProperty "upper bound can be inspected" prop_upperBoundCanBeInspected
+      , TastyHH.testProperty "lower bound can be inspected" prop_lowerBoundCanBeInspected
+      , TastyHH.testProperty "upper bound will overflow" prop_overflowsUpperBound
+      , TastyHH.testProperty "lower bound will overflow" prop_overflowsLowerBound
+      ]
+
+prop_buildsWithinBounds :: Property
+prop_buildsWithinBounds = property $ do
+  char <- forAll $ Gen.text (Range.singleton 1) Gen.unicode
+
+  let
+    oneChar :: Either BoundedText.BoundedTextError (BoundedText.BoundedText 0 1)
+    oneChar = BoundedText.boundedTextFromText char
+
+  isRight oneChar === True
+
+prop_respectsLowerBound :: Property
+prop_respectsLowerBound = withTests 1 . property $ do
+  let
+    zeroChar :: Either BoundedText.BoundedTextError (BoundedText.BoundedText 1 0)
+    zeroChar = BoundedText.boundedTextFromText ""
+
+  zeroChar === Left (BoundedText.TextLengthBelowMinimum 1)
+
+prop_respectsUpperBound :: Property
+prop_respectsUpperBound = property $ do
+  char <- forAll $ Gen.text (Range.singleton 2) Gen.unicode
+
+  let
+    exceedsMax :: Either BoundedText.BoundedTextError (BoundedText.BoundedText 0 1)
+    exceedsMax = BoundedText.boundedTextFromText char
+
+  exceedsMax === Left (BoundedText.TextLengthAboveMaximum 1)
+
+prop_upperBoundCanBeInspected :: Property
+prop_upperBoundCanBeInspected = withTests 1 . property $ do
+  let
+    bounded :: Proxy (BoundedText.BoundedText 0 0x7fff_ffff_ffff_ffff)
+    bounded = Proxy
+
+  BoundedText.boundedTextMaxLength bounded === maxBound
+
+prop_lowerBoundCanBeInspected :: Property
+prop_lowerBoundCanBeInspected = withTests 1 . property $ do
+  let
+    bounded :: Proxy (BoundedText.BoundedText 32 50)
+    bounded = Proxy
+
+  BoundedText.boundedTextMinLength bounded === 32
+
+prop_overflowsUpperBound :: Property
+prop_overflowsUpperBound = withTests 1 . property $ do
+  let
+    overflowUpperBound :: Proxy (BoundedText.BoundedText 0 0xffff_ffff_ffff_ffff) -- Overflow when cast to Int
+    overflowUpperBound = Proxy
+
+  -- We expect an overflow since we're casting to an Int
+  -- with a constant size of bits.
+  -- Int is just convenient so whatever.
+  BoundedText.boundedTextMaxLength overflowUpperBound === (-1)
+
+prop_overflowsLowerBound :: Property
+prop_overflowsLowerBound = withTests 1 . property $ do
+  let
+    negativeMinBound :: Proxy (BoundedText.BoundedText 0x8000_0000_0000_0000 0) -- Overflows when cast to Int
+    negativeMinBound = Proxy
+
+  BoundedText.boundedTextMinLength negativeMinBound === minBound
